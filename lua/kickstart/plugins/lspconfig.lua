9ffc4fbb88bc8e1c2f67167a4f823476e5c209bd
Mark <bitterjug@gmail.com>
2024-04-16 19:33:27 +0100

    Add elm language server

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -147,6 +147,7 @@ return {
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+        elmls = {},
 
         lua_ls = {
           -- cmd = {...},
